cd69b23f2e5d25e529b7d322e120d4caebdc9be2
Vaibhav <33443357+Mr-vabs@users.noreply.github.com>
2022-10-11 01:09:19 +0530

    Update Solution.py

--- Python/Solution.py
+++ Python/Solution.py
@@ -1,19 +1,36 @@
-for i in range(1,n+3):
-	for j in range(n//2+2):
-		if i<=n//2+2 and j<i:
-			print("@",end="")
-		elif i>n//2+2 and j<2*(n//2+2)-i:
-			print("@",end="")
-		else:
-			print(" ",end="")
-	for j in range(n+2):
-		if i==n//2+2:
-			print("*",end="")
-		else:
-			print(" ",end="")
-	for j in range(n):
-		if i>1 and i<n+2:
-			print("@",end="")
-	print()
+def numinput():
+	n=int(input("Number of lines 'only odd number' :: "))
+	if (n%2!=0 and n>2):
+		diode(n)
+	else:
+		print(":: Only odd numbers and not 1 ::")
+		numinput()
 
 print()
+
+def diode(n):
+	for i in range(1,n+3):
+		for j in range(n//2+2):
+			if i<=n//2+2 and j<i:
+				print("@",end="")
+			elif i>n//2+2 and j<2*(n//2+2)-i:
+				print("@",end="")
+			else:
+				print(" ",end="")
+		for j in range(n+2):
+			if i==n//2+2:
+				print("*",end="")
+			else:
+				print(" ",end="")
+		for j in range(n):
+			if i>1 and i<n+2:
+				print("@",end="")
+		print()
+	
+	print()
+	numinput()
+
+print()
+print("It's a symbol like a diode.")
+print()
+numinput()
