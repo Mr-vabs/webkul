054bccb7579e8998ed7f9d0288ae2492cbe56f4d
Vaishnvee Shinde <65480790+Vaishnvee-Shinde@users.noreply.github.com>
2022-10-18 16:04:32 +0530

    Updated Patternprint.java File

--- Java/Patternprint.java
+++ Java/Patternprint.java
@@ -1,53 +1,53 @@
 import java.util.*;
 
-class Patternprint{
-	
-
-public static void main(String[] args)
-{
-	int n, i, j;
-	Scanner s = new Scanner(System.in);
-
-	n = s.nextInt();
-	
-	for(i = 0; i < (n / 2) + 2; i++)
-	{
-		for(j = 0; j < n - i; j++)
-		{
-			System.out.print(" ");
-		}
-		for(j = 0; j < 2 * i + 1; j++)
-		{
-			System.out.print("@");
-		}
-		System.out.println();
-	}
-	
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < (n / 2) + 1; j++)
-		{
-			if (j >= n / 2 - i && j >= i - n / 2)
-			{
-				System.out.print("*");
-			}
-			else
-			System.out.print(" ");
-		}
-		for(j = 0; j < n; j++)
-		{
-			if (i == n / 2)
-				System.out.print("@");
-			else
-				System.out.print(" ");
-		}
-		for(j = 0; j < (n / 2) + 1; j++)
-		{
-			if (j >= n / 2 - i && j >= i - n / 2)
-				System.out.print("*");
-		}
-		System.out.println();
-	}
+public class Patternprint {
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+int n = s.nextInt();
+for (int i= 0; i< (n+2)/2; i++) {
+for(int j=0; j<i+1; j++) {
+System.out.print("@"+ " ");
+}
+for(int k=0; k<(2*n)-i;k++) {
+System.out.print(" "+ " ");
+}
+if(i!=0){
+for(int m=0; m<n; m++){
+System.out.print("@"+ " ");
+//  System.out.print("\n");
 }
 }
 
+System.out.print("\n");
+}
+
+for(int i=0; i<n-1; i++){
+System.out.print("@"+ " ");
+}
+for(int i=0; i<n+2; i++){
+System.out.print("*"+ " ");
+}
+for(int i=0; i<n; i++){
+System.out.print("@"+ " ");
+}
+
+System.out.print("\n");
+
+for (int i=(n+2)/2; i>0; i--) {
+for(int j=i+1; j>1; j--) {
+System.out.print("@"+ " ");
+}
+for(int k=(2*n)-i+1; k>0;k--) {
+System.out.print(" "+ " ");
+}
+if(i!=1){
+for(int m=0; m<n; m++){
+System.out.print("@"+ " ");
+// System.out.print("\n");
+}
+}
+
+System.out.print("\n");
+}
+}
+}
